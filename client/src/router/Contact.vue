1454549d66c7abb44cc90ffb804ae3e841d9f6f9
liloow <tom.beguin@gmail.com>
2017-10-29 20:10:47 +0100

    deply

--- client/src/router/Contact.vue
+++ client/src/router/Contact.vue
@@ -1,40 +1,48 @@
+<style lang="css" scoped>
+.container {
+	text-align: center;
+}
+
+</style>
 <template>
-	<div class="row">
-		<div class="columns">
-			<div class="column">
-				<img src="../assets/logo-solo.png" alt="Logo">
-			</div>
-			<div class="column">
-				<div class="row">+
-					<p>
-						N'hésitez pas à nous contacter pour toutes vos questions. Laissez-nous un message; nous y répondrons dans les plus brefs délais.
-					</p>
-					<p>
-						Tel: 0498.82 39 31
-						<br> E-mail: infos@massageme.be
-						<br> N° compte: BE13 0018 0837 4939
-					</p>
+	<div class="container">
+		<div class="row">
+			<div class="columns">
+				<div class="column">
+					<img src="../assets/logo-solo.png" alt="Logo">
 				</div>
-				<div class="row">
-					<div v-if="message">
-						<p>{{}}</p>
+				<div class="column">
+					<div class="row">
+						<p>
+							N'hésitez pas à nous contacter pour toutes vos questions. Laissez-nous un message; nous y répondrons dans les plus brefs délais.
+						</p>
+						<p>
+							Tel: 0498.82 39 31
+							<br> E-mail: infos@massageme.be
+							<br> N° compte: BE13 0018 0837 4939
+						</p>
 					</div>
-					<div>
-						<form @submit.prevent="sendForm" accept-charset="utf-8">
-							<br>
-							<input v-model="form.name" placeholder="Enter your name">
-							<br>
-							<input v-model="form.lastName" placeholder="Enter your lastName">
-							<br>
-							<input v-model="form.email" placeholder="Enter your email">
-							<br>
-							<input v-model="form.tel" placeholder="Enter your tel">
-							<br>
-							<input v-model="form.subject" placeholder="Enter your subject">
-							<br>
-							<textarea v-model="form.message" placeholder="Enter your message"></textarea>
-							<br>
-						</form>
+					<div class="row">
+						<div v-if="message">
+							<p>{{}}</p>
+						</div>
+						<div>
+							<form @submit.prevent="sendForm" accept-charset="utf-8">
+								<br>
+								<input v-model="form.name" placeholder="Enter your name">
+								<br>
+								<input v-model="form.lastName" placeholder="Enter your lastName">
+								<br>
+								<input v-model="form.email" placeholder="Enter your email">
+								<br>
+								<input v-model="form.tel" placeholder="Enter your tel">
+								<br>
+								<input v-model="form.subject" placeholder="Enter your subject">
+								<br>
+								<textarea v-model="form.message" placeholder="Enter your message"></textarea>
+								<br>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -67,7 +75,3 @@ export default {
 };
 
 </script>
-<style lang="css" scoped>
-
-
-</style>
